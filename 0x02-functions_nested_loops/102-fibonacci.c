66ad18fa76d8fd0e114afeab253ed4f3b6edb831
Dennis Kipruto Kiptoo <consraydennis77@gmail.com>
2023-06-20 17:44:37 -0700

    fibonnaci for 50 numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,17 +14,14 @@ int main(void)
 	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
-		printf("%lu", sum);
-		
+		printf("%lu", sum);		
 		fib1 = fib2;
 		fib2 = sum;
 
 		if (count == 49)
-		{
 			printf("\n");
-		} else {
+		else 
 			printf(",");
-		}
 	}
 	return (0);
 }	
