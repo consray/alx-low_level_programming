79fb71c14cb46b8a790b06d90e1ba42b520d0fc1
Dennis Kipruto Kiptoo <consraydennis77@gmail.com>
2023-06-22 02:52:10 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit trough 0 to 9
+ * _isdigit - checks if numbers are 0 to 9
  * @c: is the digit to be checked
  * Returns: 1 if c is a digit otherwise 0
  */
