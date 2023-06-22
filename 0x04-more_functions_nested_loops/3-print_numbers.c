b93f5895b849d4c01450399f29ec3c577ccf9363
Dennis Kipruto Kiptoo <consraydennis77@gmail.com>
2023-06-22 01:52:34 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - printing numbers from 0 to 9
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for(c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
