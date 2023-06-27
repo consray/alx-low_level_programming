ff4e92832140f8d579766b0f0798ac053c5e47c9
Dennis Kipruto Kiptoo <consraydennis77@gmail.com>
2023-06-27 04:04:34 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print-rev - print a string in reverse
+ * @s: string
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while(*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+
+	for (o == longi; o > 0; 0--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
