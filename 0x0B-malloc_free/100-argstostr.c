56389089d383a4429ba8d5534292de782d54c9af
Dennis Kipruto Kiptoo <consraydennis77@gmail.com>
2023-07-10 11:36:14 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -16,22 +16,30 @@ char *argstostr(int ac, char **av)
 	l = 0;
 
 	if (ac == 0 || av == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
+		{
 			l++;
+		}
 	}
 	l += ac;
 	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; av[i][n]; n++)
 	{
 		str[r] = av[i][n];
 		r++;
 	}
 	if (str[r] == '\0')
+	{
 		str[r++] = '\n';
+	}
 	return (str);
 }
