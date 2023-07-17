d0e252ceaf441d6546afedd3edf47620e6237241
Dennis Kipruto Kiptoo <consraydennis77@gmail.com>
2023-07-17 14:38:20 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - contains information of a dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: Longer Description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
