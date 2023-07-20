15cee1c4559c6af0b5bfc7419438068b6b3350b0
Dennis Kipruto Kiptoo <consraydennis77@gmail.com>
2023-07-20 02:40:04 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,16 +11,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	int sum = 0;
+	unsigned int i, sum = 0;
 
 	va_start(args, n);
-	for (int i = 0; i < n; i++)
-	{
-		if (n > 0)
-			sum += va_arg(args, int);
-		else 
-			return (0);
-	}
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
 	va_end(args);
 	return (sum);
 }
