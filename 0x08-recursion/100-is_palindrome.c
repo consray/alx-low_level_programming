9be49a3957704a083f4ce2213fd3e78a7e7307a0
Dennis Kipruto Kiptoo <consraydennis77@gmail.com>
2023-09-27 13:16:40 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -23,7 +23,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + _strlen_recusion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
 /**
  * check_pal - checks the characters recursively for palindrome
